49a26ba55ef99291fb02eccd204faaaebf6143c0
github username <github email address>
2014-06-11 16:33:00 -0400

    Scrapes education info

--- page-scraper.rb
+++ page-scraper.rb
@@ -1,5 +1,6 @@
 require 'open-uri'
 require 'nokogiri'
+require 'pry'
 
 page = Nokogiri::HTML(open('http://ruby005.students.flatironschool.com/'))
 students = page.css('li.home-blog-post') #I think this is an array
@@ -16,18 +17,18 @@ students.each do |student|
   link_to_page = student.css('div.blog-title div.big-comment h3 a').attribute('href').value
   student_page = Nokogiri::HTML(open('http://ruby005.students.flatironschool.com/' + link_to_page))
   profile_info[:blurb] = student_page.css('div.quote-div h3').text
+  bio_div = student_page.css('div.services').detect{|div| div.css('h3').text == "Biography"}
+  profile_info[:bio] = bio_div.css('p').text
   #profile_info[:bio] = student_page.css('div#ok-text-column-2 div.services p').text
-  profile_info[:edu] = student_page.css('div#ok-text-column-3 ul li').map{|school|school.text.gsub()}
+  education_div = student_page.css('div.services').detect{|div| div.css('h3').text == "Education"}
+  profile_info[:edu] = education_div.css('li').map{|school|school.text}
+  binding.pry
   student_scraped_data << profile_info
 end
 
-p student_scraped_data
+student_scraped_data[1][:edu]
 
+education_div = student_page.css('div.services').detect{|div| div.css('h3').text == "Education"}
 
-
-
-
-
-
-
-
+#Nick Demarest - line breaks but one school
+#Natalie Parellano - weird styling
