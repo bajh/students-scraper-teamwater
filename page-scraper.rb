78ed4501778d8838c0ef1451172c891eecc3537e
github username <github email address>
2014-06-11 17:18:38 -0400

    Fixes some formatting issues, primarily

--- page-scraper.rb
+++ page-scraper.rb
@@ -7,8 +7,6 @@ students = page.css('li.home-blog-post') #I think this is an array
 
 student_scraped_data = []
 
-#blurb, bio, edu, work, links
-
 students.each do |student|
   profile_info = {}
   profile_info[:name] = student.css('div.blog-title div.big-comment h3 a').text
@@ -16,20 +14,15 @@ students.each do |student|
   profile_info[:excerpt] = student.css('div.excerpt p').text
   link_to_page = student.css('div.blog-title div.big-comment h3 a').attribute('href').value
   student_page = Nokogiri::HTML(open('http://ruby005.students.flatironschool.com/' + link_to_page))
-  profile_info[:blurb] = student_page.css('div.quote-div h3').text
+  profile_info[:blurb] = student_page.css('div.quote-div h3').text.gsub!(/\\/, '')
   if bio_div = student_page.css('div.services').detect{|div| div.css('h3').text == "Biography"}
-    profile_info[:bio] = bio_div.css('p').text
+    profile_info[:bio] = bio_div.css('p').text.strip.chomp
   elsif bio_div = student_page.css('div.services').detect{|div| div.css('h3').text == "About Me"}
-    profile_info[:bio] = bio_div.css('p').text
+    profile_info[:bio] = bio_div.css('p').text.strip.chomp
   end
   education_div = student_page.css('div.services').detect{|div| div.css('h3').text == "Education"}
-  
-  profile_info[:edu] = education_div.css('li').map{|school|school.text}
+  profile_info[:edu] = education_div.css('li').map{|school|school.text.strip.chomp}
   student_scraped_data << profile_info
 end
 
-p student_scraped_data[16][:bio]
-p student_scraped_data[16][:edu]
-
-#Nick Demarest - line breaks but one school
-#Natalie Parellano - weird styling
+puts student_scraped_data
