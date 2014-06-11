a9c6aa98211d73b210b3f33eb954b4b1e75b3736
github username <github email address>
2014-06-11 15:02:32 -0400

    Grabs links to students' pages

--- page-scraper.rb
+++ page-scraper.rb
@@ -6,12 +6,17 @@ students = page.css('li.home-blog-post') #I think this is an array
 
 student_scraped_data = []
 
+#blurb, bio, edu, work, links
+
 students.each do |student|
   profile_info = {}
   profile_info[:name] = student.css('div.blog-title div.big-comment h3 a').text
-  profile_info[:tagline] = student.css('div.blog-title p.home-blog-post-meta').text
+  profile_info[:tag_line] = student.css('div.blog-title p.home-blog-post-meta').text
   profile_info[:excerpt] = student.css('div.excerpt p').text
+  link_to_page = student.css('div.blog-title div.big-comment h3 a').attribute('href').value
+  student_page = Nokogiri::HTML(open('http://ruby005.students.flatironschool.com/' + link_to_page))
+    
   student_scraped_data << profile_info
 end
 
-p student_scraped_data
+student_scraped_data
