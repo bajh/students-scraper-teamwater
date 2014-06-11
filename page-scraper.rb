ec697aaefc68818730feb6147b20fd18cd39b0ce
github username <github email address>
2014-06-11 16:47:40 -0400

    Allows scraping of education and bio for all students

--- page-scraper.rb
+++ page-scraper.rb
@@ -17,18 +17,19 @@ students.each do |student|
   link_to_page = student.css('div.blog-title div.big-comment h3 a').attribute('href').value
   student_page = Nokogiri::HTML(open('http://ruby005.students.flatironschool.com/' + link_to_page))
   profile_info[:blurb] = student_page.css('div.quote-div h3').text
-  bio_div = student_page.css('div.services').detect{|div| div.css('h3').text == "Biography"}
-  profile_info[:bio] = bio_div.css('p').text
-  #profile_info[:bio] = student_page.css('div#ok-text-column-2 div.services p').text
+  if bio_div = student_page.css('div.services').detect{|div| div.css('h3').text == "Biography"}
+    profile_info[:bio] = bio_div.css('p').text
+  elsif bio_div = student_page.css('div.services').detect{|div| div.css('h3').text == "About Me"}
+    profile_info[:bio] = bio_div.css('p').text
+  end
   education_div = student_page.css('div.services').detect{|div| div.css('h3').text == "Education"}
+  
   profile_info[:edu] = education_div.css('li').map{|school|school.text}
-  binding.pry
   student_scraped_data << profile_info
 end
 
-student_scraped_data[1][:edu]
-
-education_div = student_page.css('div.services').detect{|div| div.css('h3').text == "Education"}
+p student_scraped_data[16][:bio]
+p student_scraped_data[16][:edu]
 
 #Nick Demarest - line breaks but one school
 #Natalie Parellano - weird styling
